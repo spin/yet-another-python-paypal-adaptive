1a677fdfc68e4650dcf09e88031e440e24a03053
Spin Lai (RD-TW) <spin_lai@trend.com.tw>
2016-05-28 12:31:20 +0800

    Implemented '_build_headers()'

--- yappa/endpoints.py
+++ yappa/endpoints.py
@@ -4,14 +4,27 @@ from yappa.settings import Settings
 
 
 class AdaptiveEndpointBase(object):
+    headers = {}
+
     def __init__(self, credentials, debug=False):
         settings = Settings(debug=debug)
 
         self.endpoint = settings.PAYPAL_ENDPOINT
         self.credentials = credentials
 
+        self._build_headers()
+
     def _build_headers(self):
-        pass
+        headers = {
+            'X-PAYPAL-SECURITY-USERID': self.credentials.PAYPAL_USER_ID,
+            'X-PAYPAL-SECURITY-PASSWORD': self.credentials.PASSWORD,
+            'X-PAYPAL-SECURITY-SIGNATURE': self.credentials.PAYPAL_SIGNATURE,
+            'X-PAYPAL-APPLICATION-ID': self.credentials.PAYPAL_APP_ID,
+            'X-PAYPAL-REQUEST-DATA-FORMAT': 'JSON',
+            'X-PAYPAL-RESPONSE-DATA-FORMAT': 'JSON',
+        }
+
+        self.headers.update(headers)
 
     def _build_payload(self):
         pass
