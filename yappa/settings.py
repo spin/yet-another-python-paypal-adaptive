af6a9cceb97e1b658a188acde05584fef880877b
Spin Lai <spin_lai@trend.com.tw>
2016-05-29 18:23:50 +0800

    Added 'settings.PAYPAL_AUTH_URL'

--- yappa/settings.py
+++ yappa/settings.py
@@ -4,7 +4,10 @@ class Settings(object):
 
         if debug:
             self.PAYPAL_ENDPOINT = 'https://svcs.sandbox.paypal.com/AdaptivePayments'
+            self.PAYPAL_AUTH_URL = 'https://www.sandbox.paypal.com/cgi-bin/webscr'
             self.PAYAPL_APP_ID = 'APP-80W284485P519543T'
+
         else:
             self.PAYPAL_ENDPOINT = 'https://svcs.paypal.com/AdaptivePayments'
+            self.PAYPAL_AUTH_URL = 'https://www.paypal.com/webscr'
             self.PAYAPL_APP_ID = None
