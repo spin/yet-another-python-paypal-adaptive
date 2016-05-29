a5277637e934edaf7c30fc810479432f65d09eee
Spin Lai <spin_lai@trend.com.tw>
2016-05-29 16:47:19 +0800

    Implemented PreApproval api client.

--- yappa/apis.py
+++ yappa/apis.py
@@ -1,42 +1,99 @@
+import json
+from abc import ABCMeta, abstractproperty, abstractmethod
+from collections import namedtuple
+
 import requests
 
 from yappa.settings import Settings
 
 
-class AdaptiveApiBase(object):
+class AdaptiveApiBase(metaclass=ABCMeta):
     headers = {}
+    payload = {
+        'requestEnvelope': {
+            'errorLanguage': 'en_US',
+        }
+    }
 
     def __init__(self, credentials, debug=False):
         settings = Settings(debug=debug)
 
         self.endpoint = settings.PAYPAL_ENDPOINT
+        self.auth_url = settings.PAYPAL_AUTH_URL
         self.credentials = credentials
 
         self._build_headers()
 
     def _build_headers(self):
         headers = {
-            'X-PAYPAL-SECURITY-USERID': self.credentials.PAYPAL_USER_ID,
-            'X-PAYPAL-SECURITY-PASSWORD': self.credentials.PASSWORD,
-            'X-PAYPAL-SECURITY-SIGNATURE': self.credentials.PAYPAL_SIGNATURE,
-            'X-PAYPAL-APPLICATION-ID': self.credentials.PAYPAL_APP_ID,
+            'X-PAYPAL-SECURITY-USERID': self.credentials['PAYPAL_USER_ID'],
+            'X-PAYPAL-SECURITY-PASSWORD': self.credentials['PAYPAL_PASSWORD'],
+            'X-PAYPAL-SECURITY-SIGNATURE': self.credentials['PAYPAL_SIGNATURE'],
+            'X-PAYPAL-APPLICATION-ID': self.credentials['PAYPAL_APP_ID'],
             'X-PAYPAL-REQUEST-DATA-FORMAT': 'JSON',
             'X-PAYPAL-RESPONSE-DATA-FORMAT': 'JSON',
         }
 
         self.headers.update(headers)
 
-    def _build_payload(self):
+    def request(self, *args, **kwargs):
+        self.payload.update(self.build_payload(*args, **kwargs))
+
+        response = requests.post(self.endpoint,
+                                 data=json.dumps(self.payload),
+                                 headers=self.headers)
+
+        return self.build_response(response.json())
+
+    @abstractproperty
+    def next_url(self):
         pass
 
-    def request(self):
+    @abstractmethod
+    def build_payload(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def build_response(self, response):
         pass
 
 
 class PreApproval(AdaptiveApiBase):
+    preapproval_key = None
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.endpoint = '{}/{}'.format(self.endpoint, 'Preapproval')
+
+    @property
+    def next_url(self):
+        next_url = ''
+
+        if self.auth_url and self.preapproval_key:
+            next_url = '{}?cmd=_ap-preapproval&preapprovalkey={}'.format(
+                self.auth_url,
+                self.preapproval_key)
+
+        return next_url
+
+    def build_payload(self, *args, **kwargs):
+        return {
+            'startingDate': kwargs.get('starting_date'),
+            'endingDate': kwargs.get('ending_date'),
+            'returnUrl': kwargs.get('return_url'),
+            'cancelUrl': kwargs.get('cancel_url'),
+            'currencyCode': kwargs.get('currency'),
+            'maxAmountPerPayment': kwargs.get('max_amount_per_payment'),
+            'maxNumberOfPayments': kwargs.get('max_number_of_payments'),
+            'maxTotalAmountOfAllPayments': kwargs.get('max_total_amount_of_all_payments')
+        }
+
+    def build_response(self, response):
+        ApiResponse = namedtuple('ApiResponse', ['status', 'preapproval_key'])
+        status = response['responseEnvelope']['ack']
+        key = response['preapprovalKey']
+
+        return ApiResponse(status=status, preapproval_key=key)
 
 
 class PreApprovalDetails(AdaptiveApiBase):
@@ -46,6 +103,19 @@ class PreApprovalDetails(AdaptiveApiBase):
 
 
 class Pay(AdaptiveApiBase):
+    pay_key = None
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.endpoint = '{}/{}'.format(self.endpoint, 'Pay')
+
+    @property
+    def next_url(self):
+        next_url = ''
+
+        if self.auth_url and self.pay_key:
+            next_url = '{}?cmd=_ap-payment&paykey={}'.format(
+                self.auth_url,
+                self.pay_key)
+
+        return next_url
--- yappa/tests/test_preapproval.py
+++ yappa/tests/test_preapproval.py
@@ -1,26 +1,72 @@
+import json
 import unittest
 from unittest.mock import MagicMock, patch
 
-from yappa.endpoints import PreApproval
+from yappa.apis import PreApproval
 
 
 class PreApprovalTestCase(unittest.TestCase):
     def setUp(self):
-        self.credentials = MagicMock(
-            user='fakeuser',
-            password='fakepassword',
-            signature='1234567',
-            app_id='APP-11111',
-            return_url='',
-            cancel_url='',
-        )
+        self.credentials = {
+            'PAYPAL_USER_ID': 'fakeuserid',
+            'PAYPAL_PASSWORD': 'fakepassword',
+            'PAYPAL_SIGNATURE': '123456789',
+            'PAYPAL_APP_ID': 'APP-123456'
+        }
+
+        self.starting_date = '2016-05-28T00:33:00+08:0'
+        self.ending_date = '2016-06-28T00:33:00+08:0'
+        self.currency = 'USD'
+        self.return_url = 'http://return.url'
+        self.cancel_url = 'http://cancel.url'
+        self.max_amount_per_payment = '35.00'
+        self.max_number_of_payments = '15'
+        self.max_total_amount_of_all_payments = '500.00'
 
     def tearDown(self):
         pass
 
-    @patch('yappa.models.requests')
-    def test_request_preapproval_in_sandbox_mode(self):
-        preapproval = PreApproval(credentials=self.credentials, debug=True)
+    @patch('yappa.apis.requests')
+    def test_request_preapproval(self, mock_request):
 
-        preapproval.request()
-        # redirect_url = preapproval.authorization_url
+        expected_endpoint = 'https://svcs.sandbox.paypal.com/AdaptivePayments/Preapproval'
+        expected_headers = {
+            'X-PAYPAL-SECURITY-USERID': 'fakeuserid',
+            'X-PAYPAL-SECURITY-PASSWORD': 'fakepassword',
+            'X-PAYPAL-SECURITY-SIGNATURE': '123456789',
+            'X-PAYPAL-APPLICATION-ID': 'APP-123456',
+            'X-PAYPAL-REQUEST-DATA-FORMAT': 'JSON',
+            'X-PAYPAL-RESPONSE-DATA-FORMAT': 'JSON',
+        }
+        expected_payload = {
+            'startingDate': self.starting_date,
+            'endingDate': self.ending_date,
+            'returnUrl': self.return_url,
+            'cancelUrl': self.cancel_url,
+            'currencyCode': self.currency,
+            'maxAmountPerPayment': self.max_amount_per_payment,
+            'maxNumberOfPayments': self.max_number_of_payments,
+            'maxTotalAmountOfAllPayments': self.max_total_amount_of_all_payments,
+            'requestEnvelope': {
+                'errorLanguage': 'en_US',
+            }
+        }
+
+        preapproval = PreApproval(self.credentials, debug=True)
+
+        preapproval.request(
+            starting_date=self.starting_date,
+            ending_date=self.ending_date,
+            currency='USD',
+            return_url=self.return_url,
+            cancel_url=self.cancel_url,
+            max_amount_per_payment=self.max_amount_per_payment,
+            max_number_of_payments=self.max_number_of_payments,
+            max_total_amount_of_all_payments=self.max_total_amount_of_all_payments
+        )
+
+        args, kwargs = mock_request.post.call_args
+
+        self.assertEquals(args, (expected_endpoint,))
+        self.assertEquals(kwargs['headers'], expected_headers)
+        self.assertEquals(json.loads(kwargs['data']), expected_payload)
